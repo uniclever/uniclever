ea72c15fb5892f64caf095b1af8b9efbbf09d8d7
Justin Grierson <ju3tin2015@outlook.com>
2015-09-13 02:42:49 +0100

    sdfsdf

--- index.php
+++ index.php
@@ -3,6 +3,20 @@
 <html>
 
 <head>
+    
+    <style>#myform {
+    text-align: center;
+    padding: 5px;
+    border: 1px dotted #ccc;
+    margin: 2%;
+}
+.qty {
+    width: 40px;
+    height: 25px;
+    text-align: center;
+}
+input.qtyplus { width:25px; height:25px;}
+input.qtyminus { width:25px; height:25px;}</style>
     <title>1E-shop - OnePage one product shop</title>
     <meta charset="utf-8">
     <meta name="description" content="1E-shop - Bootstrap Theme from angelostudio.net">
@@ -24,7 +38,43 @@
 		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
-
+<script>jQuery(document).ready(function(){
+    // This button will increment the value
+    $('.qtyplus').click(function(e){
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If is not undefined
+        if (!isNaN(currentVal)) {
+            // Increment
+            $('input[name='+fieldName+']').val(currentVal + 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+    // This button will decrement the value till 0
+    $(".qtyminus").click(function(e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        fieldName = $(this).attr('field');
+        // Get its current value
+        var currentVal = parseInt($('input[name='+fieldName+']').val());
+        // If it isn't undefined or its greater than 0
+        if (!isNaN(currentVal) && currentVal > 0) {
+            // Decrement one
+            $('input[name='+fieldName+']').val(currentVal - 1);
+        } else {
+            // Otherwise put a 0 there
+            $('input[name='+fieldName+']').val(0);
+        }
+    });
+});
+</script>
 <body id="home">
     <a id="menu-link" href="#" class="">
         <span class="menu-icon"></span>
@@ -48,14 +98,14 @@
                     <div class="row">
                         <div class="col-sm-12">
                             <div class="hero-unit ">
-                                <h1 class="title"><b>1E-Shop</b> Template</h1>
-                                <h3><b>OnePage / OneProduct / OneTemplate</b></h3>  
+                                <h1 class="title"><b>Uniclever</b></h1>
+                                <h3><!--<b>OnePage / OneProduct / OneTemplate</b>--></h3>  
                                 <p><br>
-                                If you want sell only one digital product, <b>1E-shop</b> template is for you.<br>
-                                <b>1E-shop</b> is onePage full responsive bootstrap template.<br>
+                               One clever answer for all dirty questions.<br>
+                               <!-- <b>1E-shop</b> is onePage full responsive bootstrap template.<br>-->
                                 </p>
                                 <br>
-                                <a class="btn white" href="#" data-toggle="modal" data-target="#product-modal"><b>$5</b> BUY NOW</a>
+                                <a class="btn white" href="#" data-toggle="modal" data-target="#product-modal"><!--<b>$5</b>--> Create my box</a>
                             </div>
                         </div>  
                         <div class="col-sm-12 img-hero"><img src="img/logo.png" alt="#" class="zoom-img img-responsive center-block">
@@ -70,6 +120,8 @@
                 <div class="row header">
                     <div class="col-md-12">
                         <h2>PAPER CUP</h2>
+                        
+                        <br />shit happens
                         <p>HQ PSD Mock-up</p>
                     </div>
                 </div>
@@ -324,7 +376,15 @@
                     </div>
                     <div class="modal-body">
                         <h3 class="text-center"><b>PaperCup - Mockup </b>($5,00)</h3>
+                        
+                        
                     </div>
+                    
+                    <div><form id='myform' method='POST' action='#'>
+    <input type='button' value='-' class='qtyminus' field='quantity' />
+    <input type='text' name='quantity' value='0' class='qty' />
+    <input type='button' value='+' class='qtyplus' field='quantity' />
+</form></div>
                     <div class="modal-footer">
                         <form id="buy" action="buy" class="myform" method="post" novalidate>
                             <div class="form-group">
